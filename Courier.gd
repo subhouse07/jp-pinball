16d9aeb060ab8d5e024f0958fa9be08473a99238
subhouse07 <subhouse07@gmail.com>
2021-11-30 11:02:12 -0600

    Remove redundant variables from Courier node

--- Courier.gd
+++ Courier.gd
@@ -10,17 +10,15 @@ export var is_lead = false
 
 var in_slow_zone = false
 var disabled = false
-var final_courier = false
 var unit_speed = 0.13
 
 signal hit(index)
 signal captured_ball(index)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	if is_lead:
-		$StaticBody2D.collision_layer = COLLISION_LAYER_DISABLE
-		$StaticBody2D.collision_mask = COLLISION_LAYER_DISABLE
+		disabled = true
+		disable_collision()
 
 
 func _physics_process(delta):
@@ -32,44 +30,38 @@ func _physics_process(delta):
 
 
 func _on_Area2D_body_entered(body):
-	if body.name == "Ball" and !disabled and !in_slow_zone:
-		if is_lead and !final_courier:
+	if body.name == "Ball" and !in_slow_zone:
+		if is_lead and disabled:
 			emit_signal("captured_ball", index)
-		else: # Collide and disable collision
+		elif !disabled: # Collide and disable collision
 			disabled = true
 			$Sprite.hide()
 			$CollisionTimer.start()
-			
+
 
 func reset():
-	final_courier = false
-	disabled = false
 	$Sprite.show()
-	if index == 0:
+	if is_lead:
+		disabled = true
 		disable_collision()
 	else:
 		enable_collision()
-		is_lead = false
-	
 
-func set_as_lead():
-	is_lead = true
-	disable_collision()
-	
+
 func set_as_final():
-	final_courier = true
 	enable_collision()
 
+
 func enable_collision():
+	disabled = false
 	$StaticBody2D.collision_layer = COLLISION_LAYER_ENABLE
 	$StaticBody2D.collision_mask = COLLISION_LAYER_ENABLE
-	
 
-# disable this courier, emit a signal so the following courier can be made the leader of a group
+
 func disable_collision():
 	$StaticBody2D.collision_layer = COLLISION_LAYER_DISABLE
 	$StaticBody2D.collision_mask = COLLISION_LAYER_DISABLE
-	
+
 
 func _on_CollisionTimer_timeout():
 	disable_collision()
--- CourierCoordinator.gd
+++ CourierCoordinator.gd
@@ -7,6 +7,7 @@ var capture_index : int
 var capture_coords := Vector2()
 var active_path = 0
 var courier_count : int
+var paths : Node
 
 signal ball_captured
 signal ball_released
@@ -14,16 +15,17 @@ signal special_triggered
 
 
 func _ready():
-	courier_count = $CourierPaths/Path2D.get_child_count()
+	paths = $CourierPaths
+	courier_count = paths.get_node("Path2D").get_child_count()
 
 
 func _physics_process(delta):
 	if ball_captured:
-		capture_coords = $CourierPaths.get_child(active_path).get_child(capture_index).global_position
+		capture_coords = paths.get_child(active_path).get_child(capture_index).global_position
 
 
 func _on_Courier_captured_ball(index):
-	var couriers = $CourierPaths.get_child(active_path).get_children()
+	var couriers = paths.get_child(active_path).get_children()
 	for c in couriers:
 		c.disable_collision()
 	if active_path < 2:
@@ -39,16 +41,16 @@ func _on_Courier_captured_ball(index):
 func _on_Courier_hit(index):
 	courier_count -= 1
 	if courier_count <= 0:
-		courier_count = $CourierPaths.get_child(active_path).get_child_count()
+		courier_count = paths.get_child(active_path).get_child_count()
 		go_to_next_path()
 	elif courier_count == 1:
-		$CourierPaths.get_child(active_path).get_child(0).set_as_final()
+		paths.get_child(active_path).get_child(0).set_as_final()
 
 
 func _on_ReleaseArea_area_entered(area):
 	if ball_captured and area.get_parent().index == capture_index:
 		ball_captured = false
-		var couriers = $CourierPaths.get_child(active_path).get_children()
+		var couriers = paths.get_child(active_path).get_children()
 		for c in couriers:
 			if !c.disabled and !c.is_lead:
 				c.enable_collision()
@@ -62,8 +64,8 @@ func _reparent_couriers():
 	else:
 		old_index = active_path - 1
 		
-	var old_path = $CourierPaths.get_child(old_index)
-	var new_path = $CourierPaths.get_child(active_path)
+	var old_path = paths.get_child(old_index)
+	var new_path = paths.get_child(active_path)
 	var couriers = old_path.get_children()
 	
 	for i in couriers.size():
