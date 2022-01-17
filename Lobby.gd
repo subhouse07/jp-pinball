3257aa5a3019af4c17ebcca24471e4eac8ac6fe4
subhouse07 <subhouse07@gmail.com>
2022-01-17 16:20:05 -0600

    Add Lobby base node and boilerplate

---
+++ Lobby.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+enum { NETWORK, LUNCH, TRAFFIC, CENTER }
+
+var special_triggered : int
+var entrance_enabled = false
+
+signal special_entered(special_name)
+
+func _ready():
+	pass
+
+func activate_special_stage(task: int):
+	match task:
+		NETWORK:
+			pass
+		LUNCH:
+			pass
+		TRAFFIC:
+			pass
+		CENTER:
+			pass
